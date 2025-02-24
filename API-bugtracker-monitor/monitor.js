ef889250f417758373a0b3c39982da1adb924e00
Douglas Silva <dougllassillva27@gmail.com>
2025-02-24 10:10:24 -0300

    Att API2 (Octadesk)

--- API-bugtracker-monitor/monitor.js
+++ API-bugtracker-monitor/monitor.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
-const axios = require("axios");
-const express = require("express");
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const axios = require('axios');
+const express = require('express');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
@@ -36,12 +36,18 @@ async function checkAPI2() {
   try {
     const response = await axios.get(`${apiUrl2}/chat?sort[direction]=asc`, {
       headers: {
-        accept: "application/json",
-        "X-API-KEY": apiKeyOctadesk,
+        accept: 'application/json',
+        'X-API-KEY': apiKeyOctadesk,
       },
+      timeout: 60000, // Espera máxima de 60 segundos
     });
     return response.status === 200;
   } catch (error) {
+    // Verifica se o erro é de timeout
+    if (error.code === 'ECONNABORTED') {
+      console.log('🚨 Timeout atingido! A API Octadesk não respondeu em 60 segundos.');
+      await sendMessage('🚨 **API Octadesk está offline (timeout de 60 segundos)!** @everyone');
+    }
     return false;
   }
 }
@@ -52,21 +58,21 @@ async function sendMessage(message) {
     const channel = await client.channels.fetch(channelId);
     await channel.send(message);
   } catch (err) {
-    console.error("Erro ao enviar mensagem:", err);
+    console.error('Erro ao enviar mensagem:', err);
   }
 }
 
 // Comando para apagar mensagens em massa
-client.on("messageCreate", async (message) => {
-  console.log("Mensagem recebida:", message.content); // Log do conteúdo da mensagem
+client.on('messageCreate', async (message) => {
+  console.log('Mensagem recebida:', message.content); // Log do conteúdo da mensagem
 
   // Verifica se a mensagem é o comando '!clear'
-  if (message.content && message.content === "!clear") {
-    console.log("Comando !clear detectado!"); // Log quando o comando é detectado
+  if (message.content && message.content === '!clear') {
+    console.log('Comando !clear detectado!'); // Log quando o comando é detectado
 
     // Verifica se o autor da mensagem tem permissão para excluir mensagens
-    if (message.member.permissions.has("MANAGE_MESSAGES")) {
-      console.log("Permissão de MANAGE_MESSAGES verificada com sucesso."); // Log de permissão
+    if (message.member.permissions.has('MANAGE_MESSAGES')) {
+      console.log('Permissão de MANAGE_MESSAGES verificada com sucesso.'); // Log de permissão
 
       // Define o canal onde as mensagens devem ser apagadas
       const channel = message.channel;
@@ -76,29 +82,29 @@ client.on("messageCreate", async (message) => {
         let fetched;
 
         // Busca as últimas 50 mensagens
-        console.log("Buscando mensagens..."); // Log de busca
-        fetched = await channel.messages.fetch({ limit: 50 }); // Limita para 50 mensagens
+        console.log('Buscando mensagens...'); // Log de busca
+        fetched = await channel.messages.fetch({ limit: 100 }); // Limita para 20 mensagens
         console.log(`Mensagens encontradas: ${fetched.size}`); // Log do número de mensagens encontradas
 
         // Deleta as mensagens buscadas
         await channel.bulkDelete(fetched);
 
-        console.log("Mensagens deletadas com sucesso!");
-        message.channel.send("As últimas 50 mensagens foram deletadas!");
+        console.log('Mensagens deletadas com sucesso!');
+        message.channel.send('As últimas 20 mensagens foram deletadas!');
       } catch (err) {
-        console.error("Erro ao deletar mensagens:", err);
-        message.channel.send("Houve um erro ao tentar deletar as mensagens.");
+        console.error('Erro ao deletar mensagens:', err);
+        message.channel.send('Houve um erro ao tentar deletar as mensagens.');
       }
     } else {
-      console.log("Usuário sem permissão para deletar mensagens."); // Log quando não tem permissão
-      message.channel.send("Você não tem permissão para deletar mensagens.");
+      console.log('Usuário sem permissão para deletar mensagens.'); // Log quando não tem permissão
+      message.channel.send('Você não tem permissão para deletar mensagens.');
     }
   }
 });
 
 // Evento quando o bot estiver pronto
-client.once("ready", () => {
-  console.log("Bot conectado ao Discord!");
+client.once('ready', () => {
+  console.log('Bot conectado ao Discord!');
 
   // Verificação periódica a cada 15 segundos
   setInterval(async () => {
@@ -111,15 +117,15 @@ client.once("ready", () => {
     // Monitoramento da API Bugtracker
     if (!isApi1Up) {
       if (!api1WasDown) {
-        console.log("🚨 API Bugtracker está fora do ar!");
+        console.log('🚨 API Bugtracker está fora do ar!');
         api1DownTime = new Date(); // Salva o horário em que a API Bugtracker caiu
-        await sendMessage("🚨 **API Bugtracker está fora do ar!** @everyone");
+        await sendMessage('🚨 **API Bugtracker está fora do ar!** @everyone');
         api1WasDown = true;
       }
     } else {
       if (api1WasDown) {
         const downtimeDuration = calculateDowntime(api1DownTime);
-        console.log("✅ API Bugtracker voltou a funcionar!");
+        console.log('✅ API Bugtracker voltou a funcionar!');
         await sendMessage(`✅ **API Bugtracker está online novamente!** Ela ficou inativa ${downtimeDuration}`);
         api1WasDown = false;
         api1DownTime = null; // Reseta o horário de queda
@@ -129,15 +135,15 @@ client.once("ready", () => {
     // Monitoramento da API Octadesk
     if (!isApi2Up) {
       if (!api2WasDown) {
-        console.log("🚨 API Octadesk está fora do ar!");
+        console.log('🚨 API Octadesk está fora do ar!');
         api2DownTime = new Date(); // Salva o horário em que a API Octadesk caiu
-        await sendMessage("🚨 **API Octadesk está fora do ar!** @everyone");
+        await sendMessage('🚨 **API Octadesk está fora do ar!** @everyone');
         api2WasDown = true;
       }
     } else {
       if (api2WasDown) {
         const downtimeDuration = calculateDowntime(api2DownTime);
-        console.log("✅ API Octadesk voltou a funcionar!");
+        console.log('✅ API Octadesk voltou a funcionar!');
         await sendMessage(`✅ **API Octadesk está online novamente!** Ela ficou inativa por ${downtimeDuration}`);
         api2WasDown = false;
         api2DownTime = null; // Reseta o horário de queda
@@ -157,13 +163,12 @@ function calculateDowntime(downTime) {
   return `${diffHours} horas, ${diffMinutes} minutos e ${diffSeconds} segundos`;
 }
 
-
 // Inicia o servidor Express para expor o endpoint de status
 const app = express();
 const port = process.env.MONITOR_PORT || 10001; // Define a porta do monitor
 
 // Endpoint de status
-app.get("/status", (req, res) => {
+app.get('/status', (req, res) => {
   res.json({
     Bugtracker: api1Status,
     Octadesk: api2Status,
